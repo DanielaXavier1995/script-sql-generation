5f547aaf869306cf276b8c044083cebd547d369a
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-08 15:21:05 -0300

    feat: incluir db-escola

---
+++ db-escola/passo3.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_estudantes(nome, disciplina, matricula, nota1, nota2) 
+VALUES
+("Aluno 1", "matemática", "11111", 5.5, 6.0),
+("Aluno 2", "matemática", "22222", 10.0, 8.0),
+("Aluno 3", "história", "33333", 7.5, 5.5),
+("Aluno 4", "história", "44444", 8.5, 7.0),
+("Aluno 5", "matemática", "55555", 4.5, 9.5);
---
+++ db-escola/passo6.sql
@@ -0,0 +1,3 @@
+UPDATE tb_estudantes SET nota2 = 10.0 WHERE id = 3;
+
+SELECT * FROM tb_estudantes;
